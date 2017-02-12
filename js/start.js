a7dc8cea7b0c1d9ecb24e978a640746619a7c8ee
EbrahimKabodi <ealkabodi@gmail.com>
2017-02-12 10:08:44 -0800

    HackPoly submission

--- js/start.js
+++ js/start.js
@@ -4,6 +4,14 @@ $(function () {
 	var ignore_onend;
 	var final_transcript = '';
 	var start_timestamp;
+  var apiKey = 'E7V5U9-LPU643H76T'
+  var baseURL = 'http://api.wolframalpha.com/v1/simple?appid='+apiKey + "&i=";
+  //var baseURL = 'http://api.wolframalpha.com/v2/query?';//appid=' + apiKey + '&mode=default&i=';
+  //var wolfram = require('wolfram-alpha').createClient(apiKey,opts);
+  
+  //var baseURL = 'http://api.wolframalpha.com/v2/query?appid=' + apiKey + '&input=';
+  //var restURL = '&format=plaintext&async=false&reinterpret=true';
+  
 	if (!('webkitSpeechRecognition' in window)){
 		console.log("no webkit");
 	} else {
@@ -14,7 +22,7 @@ $(function () {
   		recognition.interimResults = true;
   		recognition.onstart = function() {
     	recognizing = true;
-    	$("#status_label").html("Listening... Press mic again to compute")
+    	$("#status_label").html('Listening <img src="img/listening.svg"</img> Press mic again to compute');
     	
     };
     recognition.onerror = function(event) {
@@ -39,6 +47,9 @@ $(function () {
   };
     recognition.onend = function() {
    	$("#status_label").html("Press the mic and speak");
+    if (final_transcript)
+    
+    //$("#img_result").attr("src", "img/loading.svg");
     recognizing = false;
     if (ignore_onend) {
       return;
@@ -48,6 +59,7 @@ $(function () {
       console.log('info_start');
       return;
     }
+    
   };
 
   recognition.onresult = function(event) {
@@ -66,18 +78,47 @@ $(function () {
       }
     }
     $("#equation_field").val(final_transcript);
-    console.log(final_transcript);
+    if (final_transcript.toLowerCase().endsWith("cancel")) {
+      console.log("canceling");
+      cancelRequest();
+    }
+    else if (final_transcript === "hack poly" || final_transcript === "hackpoly") {
+      updateStatusForMeme("img/hackpolymeme.png");
+    }
+    else if (final_transcript.toLowerCase() === "president trump") {
+      updateStatusForMeme("img/trumpmeme.jpg");
+    }
+    else if (final_transcript.toLowerCase() === "etcetera" || final_transcript.toLowerCase() === "etc" || final_transcript.toLowerCase() === "et cetera"){
+      updateStatusForMeme("img/etcmeme.jpg");
+    }
+
   };
 	}
 
 
 	$("#listen-btn").click(function (){
-
-		console.log(recognizing);
+    $("#meme").hide();
 		if (recognizing) {
 			$("#listen-btn").removeClass("red");
+      if (final_transcript.length != 0){
+        var query = encodeURIComponent(final_transcript.toLowerCase());
+        $("#loading_gif").show();
+        $("#img_result").show();
+        $("#img_result").attr("src", baseURL+query);
+        $("#img_result")
+            .on('load', function() { $("#loading_gif").hide(); })
+            .on('error', function() { 
+              $("#loading_gif").hide();
+              $("#img_result").hide(); 
+              $("#error_message").show();});
+      }
+      
+   
 			recognition.stop();
-		} else{
+      recognizing = false;
+		} else {
+      $("#error_message").hide();
+      $("#loading_gif").hide();
 			$("#listen-btn").addClass("red");
 			final_transcript = '';
 			$("#equation_field").val("");
@@ -88,6 +129,24 @@ $(function () {
 
 	});
 
+  function updateStatusForMeme(memeSrc) {
+      $("#loading_gif").hide();
+      recognition.stop();
+      $("#listen-btn").removeClass("red");
+      $("#meme").attr('src', memeSrc);
+      var n = $(document).height();
+      $('html, body').animate({ scrollBottom: n }, 10);
+      $("#meme").animate({
+            height: 'toggle'
+        });
+      recognizing = false;
+      $("#img_result").hide();
+  }
+  function cancelRequest() {
+    recognition.stop();
+    $("#listen-btn").removeClass("red");
+    recognizing = false;
+  }
 
 
 
